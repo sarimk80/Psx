378eb27aea463065ffabc87821357154113e7234
Sarim <sarimk80@gmail.com>
2026-04-02 12:44:48 +0500

    update build no.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,8 +18,8 @@ android {
         applicationId = "com.pizza.psx"
         minSdk = 33
         targetSdk = 35
-        versionCode = 17
-        versionName = "2.6"
+        versionCode = 18
+        versionName = "2.7"
 
 
 
